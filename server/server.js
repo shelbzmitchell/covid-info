7aabcb5beab940b01379656dc0b224aa4774c437
Brian Alves <brianthomasalves@gmail.com>
2020-04-20 16:46:22 -0700

    added server structure

---
+++ server/server.js
@@ -0,0 +1,9 @@
+/* app.js is the project's entry point */
+const axios = require("axios");
+
+/*
+ * Create express web server
+ */
+const express = require("express");
+const app = express();
+const port = 5000;
