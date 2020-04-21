2c49ed29e69860bd0e1d559f8043f294306c3802
Shelby Mitchell <shelbzmitchell@gmail.com>
2020-04-21 16:08:23 -0700

    back-end api call

--- server/server.js
+++ server/server.js
@@ -7,3 +7,18 @@ const axios = require("axios");
 const express = require("express");
 const app = express();
 const port = 5000;
+
+// axios call
+
+axios
+  .get("https://bing.com/covid/data")
+  .then((response) => {
+    console.log(response.data.totalRecovered);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.listen(5000, () => {
+  console.log("server running on port 5000");
+});
